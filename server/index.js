013ebfd86f64e343d94e86232a6a3f47fd27f988
smit <smitrakholiya0000@gmail.com>
2025-02-26 22:28:01 +0530

    serverside besic folders setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
+require("dotenv").config();
+const express = require('express')
+const cors = require("cors");
+const app = express()
+const port = 3000
+// const authRouter = require('./router/auth-router')
+// const adminRouter = require("./router/admin-router")
+const connectDb= require('./utils/db')
 
-app.get("/", (req, res) => {
-  res.send("Hello, Node.js Server!");
-});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+var corsOptions = {
+    origin: 'http://localhost:5173',
+    methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
+    credential: true,
+  }
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
+// app.use("/api/auth", authRouter)
+// app.use("/api/admin", adminRouter)
+
+
+connectDb().then(() => { 
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})})
+
---
+++ server/utils/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const dbURI = process.env.MONGO_URI
+const connectDb = async()=>{
+  try {
+    await mongoose.connect(dbURI);
+  } 
+  catch (error) {
+    console.log('Error connecting to MongoDB', error);
+    process.exit(0);
+  }}
+
+module.exports = connectDb; 
